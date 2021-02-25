f7dea2759f674d194eb6269380afe56a866d3a6c
Barnes, Dan <dbarnes1@paychex.com>
2021-02-25 13:33:34 -0500

    chore: remove unused build dependencies

--- babel.config.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-    "presets": [["@babel/env", { "modules": false }]],
-    "plugins": ["@babel/plugin-proposal-class-properties"]
-}
--- rollup.config.js
+++ rollup.config.js
@@ -1,38 +1,7 @@
 const { nodeResolve } = require("@rollup/plugin-node-resolve");
-const { terser } = require("rollup-plugin-terser");
-const polyfills = require('rollup-plugin-node-polyfills');
 const commonjs = require('@rollup/plugin-commonjs');
-const { babel } = require("@rollup/plugin-babel");
-
-const pkg = require('./package.json');
 
 module.exports = [
-    {
-        // UMD
-        input: 'index.mjs',
-        plugins: [
-            nodeResolve({
-                browser: true,
-                preferBuiltins: false,
-            }),
-            commonjs({
-                include: /node_modules/,
-            }),
-            babel({
-                babelHelpers: "bundled",
-            }),
-            polyfills(),
-            terser(),
-        ],
-        output: {
-            file: `dist/paychex.collector-azure.min.js`,
-            format: "umd",
-            name: pkg.name,
-            esModule: false,
-            exports: "named",
-            sourcemap: true,
-        },
-    },
     // ESM and CJS
     {
         input: 'index.mjs',

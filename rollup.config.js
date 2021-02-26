d04b117a0ef15a45611d37fcfbd4d553d23a7cae
Barnes, Dan <dbarnes1@paychex.com>
2021-02-26 13:19:16 -0500

    fix: replace lodash-es with lodash in cjs

--- rollup.config.js
+++ rollup.config.js
@@ -1,12 +1,36 @@
 const { nodeResolve } = require("@rollup/plugin-node-resolve");
 const commonjs = require('@rollup/plugin-commonjs');
+const replace = require('@rollup/plugin-replace');
 
 module.exports = [
-    // ESM and CJS
+    // ESM
     {
         input: 'index.mjs',
-        external: ['lodash-es'],
+        external: ['lodash-es', '@azure/event-hubs'],
         plugins: [
+            nodeResolve({
+                preferBuiltins: true
+            }),
+            commonjs({
+                include: /node_modules/,
+            })
+        ],
+        output: {
+            dir: "dist/esm",
+            format: "esm",
+            exports: "named",
+            sourcemap: true,
+        },
+    },
+    // CJS
+    {
+        input: 'index.mjs',
+        external: ['lodash', '@azure/event-hubs'],
+        plugins: [
+            replace({
+                'lodash-es': 'lodash',
+                preventAssignment: true,
+            }),
             nodeResolve({
                 preferBuiltins: true,
             }),
@@ -14,19 +38,11 @@ module.exports = [
                 include: /node_modules/,
             })
         ],
-        output: [
-            {
-                dir: "dist/esm",
-                format: "esm",
-                exports: "named",
-                sourcemap: true,
-            },
-            {
-                dir: "dist/cjs",
-                format: "cjs",
-                exports: "named",
-                sourcemap: true,
-            },
-        ],
+        output: {
+            dir: "dist/cjs",
+            format: "cjs",
+            exports: "named",
+            sourcemap: true,
+        },
     },
 ];
